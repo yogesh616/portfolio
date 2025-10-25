7f57c314e9b526519bfb10d943aea482a047fd6f
yogesh616 <ys30371@gmail.com>
2025-10-25 10:50:53 +0530

    Fix  conflicts

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -9,15 +7,4 @@ createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-=======
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
->>>>>>> 6e496f4f203f2382944ac644fbeaae2c7eef8cc4
 )
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-<<<<<<< HEAD
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),  tailwindcss()],
-=======
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
->>>>>>> 6e496f4f203f2382944ac644fbeaae2c7eef8cc4
+
 })
